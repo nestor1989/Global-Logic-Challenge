cc4caa115b525b1a160393ad08185dbe232f487c
ihsa-nestor-delrio <ndelrio@emergencias.com.ar>
2022-06-28 19:10:08 -0300

    added project structure

---
+++ app/src/main/java/com/idea3d/global_logic_challenge/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.idea3d.global_logic_challenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
